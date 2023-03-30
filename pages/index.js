0bfdd2b423a84bbaf70b7ff98f03bb6fa99b859a
shivanshi <shivanshi.saxena2@gmail.com>
2023-03-30 23:49:18 +0530

    basic front layout completed

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,8 @@ import Navbar from '@/components/Navbar'
 import Main from '@/components/Main'
 import About from '@/components/About'
 import Skills from '@/components/Skills'
+import Projects from '@/components/Projects'
+import Contact from '@/components/Contact'
 
 
 const inter = Inter({ subsets: ['latin'] })
@@ -22,6 +24,8 @@ export default function Home() {
       <Main />
       <About/>
       <Skills/>
+      <Projects/>
+      <Contact/>
     </div>
   )
 }
